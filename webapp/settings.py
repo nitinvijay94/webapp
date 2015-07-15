e9dbad2c9cdc4ff4bd7060921a0c297467a06fd1
ding xiong <dingxiong203@gmail.com>
2015-07-15 14:15:38 -0400

    add simple serializers

--- webapp/settings.py
+++ webapp/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'vendor'
+    'vendor',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
