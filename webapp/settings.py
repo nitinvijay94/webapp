9dd1fe0b6e0e731d0fac97d0acf9d4c700e14383
ding xiong <dingxiong203@gmail.com>
2015-07-14 21:38:16 -0400

    3

--- webapp/settings.py
+++ webapp/settings.py
@@ -65,8 +65,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = '/media/sf_share/webapp/'
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = '/media/sf_share/webapp/'
+#MEDIA_URL = '/media/'
 
 ############################################################
 # added according to heroku
