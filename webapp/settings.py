2b1cd344b4e82f5d8380bc2270aaa5f7c8f32001
ding xiong <dingxiong203@gmail.com>
2015-07-14 21:53:05 -0400

    3

--- webapp/settings.py
+++ webapp/settings.py
@@ -79,10 +79,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
